db14a2e776ed8e5bb3fc1c1031ad047fcc26609a
Hardheaddinero <shinekwasi2@yahoo.com>
2023-07-03 17:04:08 +0000

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_chessboard - a program that prints the chessboard
+ *@a: parameter to print
+ *Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m, n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[m][n]);
+		}
+		_putchar('\n');
+	}
+}
